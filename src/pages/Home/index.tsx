0105f2353777d397f53f653fff3b8eda26d0e5e1
mateus-code-98 <teu-9@hotmail.com>
2021-11-28 18:18:47 -0300

    sada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -51,7 +51,187 @@ export const Home: React.FC = () => {
                 </View>
             </View>
             <View style={{ justifyContent: 'space-between', flex: 1 }}>
-                <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1 }}>
+                    <View style={{ flexDirection: "row", marginBottom: 10, borderBottomWidth: 1, borderBottomColor: "#1D2766", paddingBottom: 10 }}>
+                        <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROFMtsmWYQIC_g4m6iEkdtSsly9B72-0D6QQ&usqp=CAU" }} />
+                        <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ justifyContent: "space-between", marginLeft: 20 }}>
+                                <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 24, color: "#DDE3F0" }}>
+                                    Tiago Luchtenberg
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: "#32BD50", marginRight: 5 }}>
+                                    </View>
+                                    <Text style={{ fontFamily: "Rajdhani_400Regular", fontSize: 13, color: "#ABB1CC" }}>Disponível</Text>
+                                </View>
+                            </View>
+                            <View style={{ justifyContent: "center" }}>
+                                <RectButton onPress={() => navigator.navigate('Game')} style={{ padding: 5, backgroundColor: "#E51C44", borderRadius: 3 }}>
+                                    <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 18, color: "#DDE3F0" }}>Jogar</Text>
+                                </RectButton>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: "row", marginBottom: 10, borderBottomWidth: 1, borderBottomColor: "#1D2766", paddingBottom: 10 }}>
+                        <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_IpQW139PUiN8X-E7DwUyT3VHeU1yYvOchA&usqp=CAU" }} />
+                        <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ justifyContent: "space-between", marginLeft: 20 }}>
+                                <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 24, color: "#DDE3F0" }}>
+                                    Rodrigo Gonçalves
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: "#32BD50", marginRight: 5 }}>
+                                    </View>
+                                    <Text style={{ fontFamily: "Rajdhani_400Regular", fontSize: 13, color: "#ABB1CC" }}>Disponível</Text>
+                                </View>
+                            </View>
+                            <View style={{ justifyContent: "center" }}>
+                                <RectButton onPress={() => navigator.navigate('Game')} style={{ padding: 5, backgroundColor: "#E51C44", borderRadius: 3 }}>
+                                    <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 18, color: "#DDE3F0" }}>Jogar</Text>
+                                </RectButton>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: "row", marginBottom: 10 }}>
+                        <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVETOq-mhgUhMfQKHgk7xVqUf_f_bVa_srMA&usqp=CAU" }} />
+                        <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ justifyContent: "space-between", marginLeft: 20 }}>
+                                <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 24, color: "#DDE3F0" }}>
+                                    Diego Fernandes
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: "#32BD50", marginRight: 5 }}>
+                                    </View>
+                                    <Text style={{ fontFamily: "Rajdhani_400Regular", fontSize: 13, color: "#ABB1CC" }}>Disponível</Text>
+                                </View>
+                            </View>
+                            <View style={{ justifyContent: "center" }}>
+                                <RectButton onPress={() => navigator.navigate('Game')} style={{ padding: 5, backgroundColor: "#E51C44", borderRadius: 3 }}>
+                                    <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 18, color: "#DDE3F0" }}>Jogar</Text>
+                                </RectButton>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: "row", marginBottom: 10, borderBottomWidth: 1, borderBottomColor: "#1D2766", paddingBottom: 10 }}>
+                        <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROFMtsmWYQIC_g4m6iEkdtSsly9B72-0D6QQ&usqp=CAU" }} />
+                        <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ justifyContent: "space-between", marginLeft: 20 }}>
+                                <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 24, color: "#DDE3F0" }}>
+                                    Tiago Luchtenberg
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: "#32BD50", marginRight: 5 }}>
+                                    </View>
+                                    <Text style={{ fontFamily: "Rajdhani_400Regular", fontSize: 13, color: "#ABB1CC" }}>Disponível</Text>
+                                </View>
+                            </View>
+                            <View style={{ justifyContent: "center" }}>
+                                <RectButton onPress={() => navigator.navigate('Game')} style={{ padding: 5, backgroundColor: "#E51C44", borderRadius: 3 }}>
+                                    <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 18, color: "#DDE3F0" }}>Jogar</Text>
+                                </RectButton>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: "row", marginBottom: 10, borderBottomWidth: 1, borderBottomColor: "#1D2766", paddingBottom: 10 }}>
+                        <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_IpQW139PUiN8X-E7DwUyT3VHeU1yYvOchA&usqp=CAU" }} />
+                        <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ justifyContent: "space-between", marginLeft: 20 }}>
+                                <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 24, color: "#DDE3F0" }}>
+                                    Rodrigo Gonçalves
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: "#32BD50", marginRight: 5 }}>
+                                    </View>
+                                    <Text style={{ fontFamily: "Rajdhani_400Regular", fontSize: 13, color: "#ABB1CC" }}>Disponível</Text>
+                                </View>
+                            </View>
+                            <View style={{ justifyContent: "center" }}>
+                                <RectButton onPress={() => navigator.navigate('Game')} style={{ padding: 5, backgroundColor: "#E51C44", borderRadius: 3 }}>
+                                    <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 18, color: "#DDE3F0" }}>Jogar</Text>
+                                </RectButton>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: "row", marginBottom: 10 }}>
+                        <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVETOq-mhgUhMfQKHgk7xVqUf_f_bVa_srMA&usqp=CAU" }} />
+                        <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ justifyContent: "space-between", marginLeft: 20 }}>
+                                <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 24, color: "#DDE3F0" }}>
+                                    Diego Fernandes
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: "#32BD50", marginRight: 5 }}>
+                                    </View>
+                                    <Text style={{ fontFamily: "Rajdhani_400Regular", fontSize: 13, color: "#ABB1CC" }}>Disponível</Text>
+                                </View>
+                            </View>
+                            <View style={{ justifyContent: "center" }}>
+                                <RectButton onPress={() => navigator.navigate('Game')} style={{ padding: 5, backgroundColor: "#E51C44", borderRadius: 3 }}>
+                                    <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 18, color: "#DDE3F0" }}>Jogar</Text>
+                                </RectButton>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: "row", marginBottom: 10, borderBottomWidth: 1, borderBottomColor: "#1D2766", paddingBottom: 10 }}>
+                        <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROFMtsmWYQIC_g4m6iEkdtSsly9B72-0D6QQ&usqp=CAU" }} />
+                        <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ justifyContent: "space-between", marginLeft: 20 }}>
+                                <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 24, color: "#DDE3F0" }}>
+                                    Tiago Luchtenberg
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: "#32BD50", marginRight: 5 }}>
+                                    </View>
+                                    <Text style={{ fontFamily: "Rajdhani_400Regular", fontSize: 13, color: "#ABB1CC" }}>Disponível</Text>
+                                </View>
+                            </View>
+                            <View style={{ justifyContent: "center" }}>
+                                <RectButton onPress={() => navigator.navigate('Game')} style={{ padding: 5, backgroundColor: "#E51C44", borderRadius: 3 }}>
+                                    <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 18, color: "#DDE3F0" }}>Jogar</Text>
+                                </RectButton>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: "row", marginBottom: 10, borderBottomWidth: 1, borderBottomColor: "#1D2766", paddingBottom: 10 }}>
+                        <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_IpQW139PUiN8X-E7DwUyT3VHeU1yYvOchA&usqp=CAU" }} />
+                        <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ justifyContent: "space-between", marginLeft: 20 }}>
+                                <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 24, color: "#DDE3F0" }}>
+                                    Rodrigo Gonçalves
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: "#32BD50", marginRight: 5 }}>
+                                    </View>
+                                    <Text style={{ fontFamily: "Rajdhani_400Regular", fontSize: 13, color: "#ABB1CC" }}>Disponível</Text>
+                                </View>
+                            </View>
+                            <View style={{ justifyContent: "center" }}>
+                                <RectButton onPress={() => navigator.navigate('Game')} style={{ padding: 5, backgroundColor: "#E51C44", borderRadius: 3 }}>
+                                    <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 18, color: "#DDE3F0" }}>Jogar</Text>
+                                </RectButton>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: "row", marginBottom: 10 }}>
+                        <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVETOq-mhgUhMfQKHgk7xVqUf_f_bVa_srMA&usqp=CAU" }} />
+                        <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
+                            <View style={{ justifyContent: "space-between", marginLeft: 20 }}>
+                                <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 24, color: "#DDE3F0" }}>
+                                    Diego Fernandes
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: "#32BD50", marginRight: 5 }}>
+                                    </View>
+                                    <Text style={{ fontFamily: "Rajdhani_400Regular", fontSize: 13, color: "#ABB1CC" }}>Disponível</Text>
+                                </View>
+                            </View>
+                            <View style={{ justifyContent: "center" }}>
+                                <RectButton onPress={() => navigator.navigate('Game')} style={{ padding: 5, backgroundColor: "#E51C44", borderRadius: 3 }}>
+                                    <Text style={{ fontFamily: "Rajdhani_600SemiBold", fontSize: 18, color: "#DDE3F0" }}>Jogar</Text>
+                                </RectButton>
+                            </View>
+                        </View>
+                    </View>
                     <View style={{ flexDirection: "row", marginBottom: 10, borderBottomWidth: 1, borderBottomColor: "#1D2766", paddingBottom: 10 }}>
                         <Image style={{ height: 50, width: 50, borderRadius: 3 }} source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROFMtsmWYQIC_g4m6iEkdtSsly9B72-0D6QQ&usqp=CAU" }} />
                         <View style={{ flexDirection: "row", justifyContent: 'space-between', flex: 1 }}>
